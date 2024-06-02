b21c01190e7d5f8e80c7b55eb0aae1e548e893dc
matriyeatul <atul.naik@matriyeacademy.com>
2024-06-02 23:29:39 +0530

    express and app configuration

--- src/index.js
+++ src/index.js
@@ -1,31 +1,42 @@
 // Import the dotenv package
 import dotenv from "dotenv";
-import connectDB from "./db/index.js";
 // Configure the dotenv package to load environment variables from the .env file
 dotenv.config({
   path: "./.env",
 });
-connectDB();
-// import mongoose from "mongoose";
-// import express from "express";
-// const app = express();
-// import { DB_NAME } from "./constants.js";
-// // Import the dotenv package
-// import dotenv from "dotenv";
-// // Configure the dotenv package to load environment variables from the .env file
-// dotenv.config();
 
-// (async () => {
-//   try {
-//     await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`);
-//     console.log("MongoDB connected successfully");
-//     app.on("error", (error) => {
-//       console.error("ERROR: ", error);
-//     });
-//     app.listen(process.env.PORT, () => {
-//       console.log(`App is litening on port::${process.env.PORT}`);
-//     });
-//   } catch (error) {
-//     console.error("ERROR: ", error);
-//   }
-// })();
+import connectDB from "./db/index.js";
+import express from "express";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+const app = express();
+
+app.use(
+  cors({
+    origin: process.env.CORS_ORIGIN, // The environment variable is now available
+    credentials: true,
+  })
+);
+
+app.use(
+  express.json({
+    limit: "16kb",
+  })
+);
+
+app.use(express.urlencoded({ extended: true, limit: "16kb" }));
+
+app.use(express.static("public"));
+
+app.use(cookieParser());
+
+const port = process.env.PORT || 8000;
+connectDB()
+  .then(() => {
+    app.listen(port, () => {
+      console.log(`Server is running on ${port}`);
+    });
+  })
+  .catch((err) => {
+    console.error("MONGODB connection failed!!! ", err);
+  });
